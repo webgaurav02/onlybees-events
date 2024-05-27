37d22e46fb75a7812d4b30dd07b4075a33338362
webgaurav02 <gauravcodes123@gmail.com>
2024-05-27 14:05:22 +0530

    Admin login page ui done

--- src/app/admin/login/page.js
+++ src/app/admin/login/page.js
@@ -1,8 +1,15 @@
 // src/app/login/page.js
 'use client';
 
+// Images
+import logo from "../../../../public/OnlyBees_light.svg";
+
+
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from "next/link";
+
 
 export default function LoginPage() {
   const [username, setUsername] = useState('');
@@ -26,27 +33,58 @@ export default function LoginPage() {
   }
 
   return (
-    <div>
-      <h1>Admin Login</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Username:</label>
-          <input
-            type="text"
-            value={username}
-            onChange={(e) => setUsername(e.target.value)}
+    <section className="bg-gray-50 dark:dark:bg-[#121212]">
+      <div className="flex flex-col items-center lg:justify-center px-6 py-8 mx-auto h-screen lg:py-0">
+        <Link href="/" className="flex items-center mb-10 mt-16 lg:mt-0 text-2xl font-semibold text-gray-900 dark:text-white">
+          <Image
+            className="mr-2"
+            src={logo}
+            width={300}
+            height="auto"
+            alt="Onlybees logo"
           />
+        </Link>
+        <div className="w-full bg-white rounded-lg shadow md:mt-0 sm:max-w-md xl:p-0 dark:bg-[#1e1e1e]">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className=" text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Admin Login
+            </h1>
+            <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">username</label>
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#1e1e1e] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="username"
+                  required
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">password</label>
+                <input
+                  type="password"
+                  name="password"
+                  id="password"
+                  placeholder="••••••••"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-[#1e1e1e] dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                </div>
+              </div>
+              <button type="submit" className="w-full text-black bg-[#00FF38] focus:ring-4 focus:outline-none focus:ring-primary-300 font-semibold rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+            </form>
+          </div>
         </div>
-        <div>
-          <label>Password:</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-        </div>
-        <button type="submit">Login</button>
-      </form>
-    </div>
+      </div>
+    </section>
   );
 }
