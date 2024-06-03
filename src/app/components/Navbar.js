2d67dc9d20662ea56a920a9bbc039961d060c8e1
webgaurav02 <gauravcodes123@gmail.com>
2024-06-03 10:45:49 +0530

    Modify navbar

--- src/app/components/Navbar.js
+++ src/app/components/Navbar.js
@@ -1,22 +1,59 @@
+"use client"
 // import "./Navbar.css";
 import Link from "next/link";
+import Image from "next/image"
+import { useState } from "react";
+import { usePathname } from 'next/navigation'
+import { FaBars, FaTimes } from 'react-icons/fa';
+
+//Assets
 import dark from "../../../public/OnlyBees_dark.svg";
 import light from "../../../public/OnlyBees_light.svg";
-import Image from "next/image"
+
 
 const Navbar = (props) => {
+
+    const pathname = usePathname()
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+
     return (
-        <div className={`nav border-b border-solid ${props.mode=="dark"?"border-white bg-black":"border-black bg-white"} lg:mx-10 py-7 mx-5`}>
-            <Link href="/" passHref>
-                <Image
-                    src={(props.mode=="dark")?light:dark}
-                    width={180}
-                    height="auto"
-                    alt="OnlyBees logo"
-                />
-            </Link>
-        </div>
+        <nav className={`${props.mode == "dark" ? "bg-black text-white border-white" : "bg-none border-black text-black"} nav border-b border-solid md:mx-10 py-5 mx-5 md:pr-2`}>
+            <div className="flex justify-between items-center">
+                <Link href="/" passHref>
+                    <Image
+                        src={(props.mode == "dark") ? light : dark}
+                        width={150}
+                        height="auto"
+                        alt="OnlyBees logo"
+                    />
+                </Link>
+                <div className="md:hidden" onClick={toggleMenu}>
+                    {!isOpen && <FaBars className="text-2xl" />}
+                </div>
+                <div className={`md:flex flex-col md:flex-row items-center md:gap-3 fixed md:static top-0 right-0 h-full md:h-auto ${props.mode == "dark" ? "bg-black text-white" : "bg-[#D9D9D9] text-black"} md:bg-transparent transform ${isOpen ? 'translate-x-0' : 'translate-x-full'} md:translate-x-0 transition-transform duration-300 ease-in-out w-[70vw] md:w-auto`}>
+                    <div className="flex justify-end w-full md:hidden p-4">
+                        <FaTimes className="text-2xl cursor-pointer" onClick={toggleMenu} />
+                    </div>
+                    <Link href="/events" className={`md:text-sm block md:px-4 py-3 ml-10 md:ml-0 md:py-0 ${pathname === '/events' ? 'underline underline-offset-8' : ''}`}>Browse events</Link>
+                    <Link href="/about" className={`md:text-sm block md:px-4 py-3 ml-10 md:ml-0 md:py-0 ${pathname === '/about' ? 'underline underline-offset-8' : ''}`}>About</Link>
+                    <Link href="/shop" className={`md:text-sm block md:px-4 py-3 ml-10 md:ml-0 md:py-0 ${pathname === '/shop' ? 'underline underline-offset-8' : ''}`}>Shop</Link>
+                    <Link href="/artist" className={`md:text-sm block md:px-4 py-3 ml-10 md:ml-0 md:py-0 ${pathname === '/artist' ? 'underline underline-offset-8' : ''}`}>Artist</Link>
+                    <Link href="/blog" className={`md:text-sm block md:px-4 lg:pr-6 py-3 ml-10 md:ml-0 md:py-0 ${pathname === '/blog' ? 'underline underline-offset-8' : ''}`}>Blog</Link>
+                    <div className="text-center mt-20 lg:mt-0">
+                        <Link href="/login" className={`md:text-xs ${props.mode == "dark" ? "bg-white text-black" : "bg-black text-white"} lg:px-8 px-12 py-2 rounded-full`} >Login</Link>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
     )
 }
 
 export default Navbar;
+
+
